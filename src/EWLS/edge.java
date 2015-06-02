2056499313c34919464962b3487b8cf973710a31
FlyHighest <zhangtianyu@buaa.edu.cn>
2015-06-02 21:29:09 +0800

    pakage name

---
+++ src/EWLS/edge.java
@@ -0,0 +1,17 @@
+package EWLS;
+
+public class edge {
+	int x, y;
+
+	public edge(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	public boolean equals(edge another){
+		if(this.x==another.x&&this.y==another.y)
+			return true;
+		if(this.x==another.y&&this.y==another.x)
+			return true;
+		return false;
+	}
+}
