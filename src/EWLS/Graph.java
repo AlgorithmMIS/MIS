ad89f27c941bc492561b0bf980fda1180bef19ab
wangxiaoyaner <1162752934@qq.com>
2015-06-07 18:47:58 +0800

    Update Graph.java

--- src/EWLS/Graph.java
+++ src/EWLS/Graph.java
@@ -1,21 +1,25 @@
 package EWLS;
 //package EWLS;
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Hashtable;
 import java.util.Scanner;
 
 public class Graph {
-	HashMap<Integer,HashSet<edge>>adTable;
-	HashMap<Integer,HashSet<Integer>>adis=new HashMap<Integer,HashSet<Integer>>();
-//	ArrayList<HashSet<edge>> adTable;
+	HashMap<Integer,HashSet<edge>> adTable;
 	HashSet<Integer> orivs = new HashSet<Integer>();
 	edges oriedges = new edges();
 	int numberOfVertex;
+	//HashMap<edge, Integer> weigth = new HashMap<edge, Integer>();
+
 	public Graph() {
-		System.out.println("Hello");
-		this.adTable = new HashMap<Integer,HashSet<edge>>();
+	//	System.out.println("Hello");
+		this.adTable = new HashMap< Integer,HashSet<edge>>();
 		File file = new File("src/frb100-40.mis");
 		String line = null;
 		edge temp;
@@ -27,9 +31,7 @@ public class Graph {
 			strs = line.split(" ");
 			this.numberOfVertex = Integer.valueOf(strs[2]);
 			for(int i=0;i<numberOfVertex;i++){
-				adTable.put(i, new HashSet<edge>());
-				adis.put(i, new HashSet<Integer>());
-			//	adTable.add(new HashSet<edge>());
+				adTable.put(i,new HashSet<edge>());
 			}
 			while (br.hasNextLine()) {
 				line = br.nextLine();
@@ -41,8 +43,7 @@ public class Graph {
 				temp = new edge(v1, v2);
 				//weigth.put(temp, 1);// 初始权重
 				oriedges.add(temp);
-				adis.get(v1).add(v2);
-				adis.get(v2).add(v1);
+				
 				adTable.get(v1).add(temp);
 				adTable.get(v2).add(temp);
 			}
@@ -52,4 +53,5 @@ public class Graph {
 		}
 	}
 }
+
 
